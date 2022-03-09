a173c8253eabd042baef53838e5e0079fcb40189
zufar-ik <zufartangribergenov1122@gmail.com>
2022-03-09 22:09:05 +0500

    ADD

---
+++ data/config.py
@@ -0,0 +1,11 @@
+from environs import Env
+
+# Используем библиотеку environs
+env = Env()
+env.read_env()
+
+# Считываем данные из .env
+
+BOT_TOKEN = "asklcasckamsc"  # Токен бота
+
+ADMINS = [123848]  # Список админов
---
+++ keyboards/default/__init__.py
@@ -0,0 +1,5 @@
+from . import buttons
+from . import apple
+from . import galaxy
+from . import model_redmi
+from . import pixel
