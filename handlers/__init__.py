e16bc5a551608215564fd418a5223d2b365cf156
zufar-ik <zufartangribergenov1122@gmail.com>
2022-03-20 10:49:15 +0500

    fix

--- handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import users
-from . import errors
-from . import groups
-from . import channels
--- keyboards/default/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import buttons
-from . import apple
-from . import galaxy
-from . import model_redmi
-from . import pixel
