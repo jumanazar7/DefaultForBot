16ba72208519722b06df75d1d10f0cdccfe43e67
zufar_ik <97404942+zufar-ik@users.noreply.github.com>
2022-07-18 13:38:13 +0500

    Create state.py

---
+++ states/state.py
@@ -0,0 +1,8 @@
+class Reklama(StatesGroup):
+    reklama = State()
+    reklama_2 = State()
+    reklama_3 = State()
+    reklama_4 = State()
+    reklama_5 = State()
+    reklama_6 = State()
+    reklama_7 = State()
